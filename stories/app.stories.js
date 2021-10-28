a1657c75c277ae530daef19d626e7ccd8d7bc004
Kathryn Simpson <kathryndsimpson@gmail.com>
2021-10-28 10:58:31 -0400

    Stroybook Update

--- stories/app.stories.js
+++ stories/app.stories.js
@@ -9,18 +9,19 @@ export default {
   },
 };
 
-
-function Template({ type = "math", slot }) {
-  return html`
-    <learning-card type="${type}">
-    ${slot}
-    </learning-card>
-  `;
+function Template({ type = 'math', slot }) {
+  return html` <learning-card type="${type}"> ${slot} </learning-card> `;
 }
-export const Card = Template.bind({});
+export const MathCard = Template.bind({});
 
 export const ScienceCard = Template.bind({});
 ScienceCard.args = {
   type: 'science',
-  slot: html`<p>slotted content that should render</p>`
+  slot: html`<p>slotted content that should render</p>`,
+};
+
+export const TechnologyCard = Template.bind({});
+TechnologyCard.args = {
+  type: 'technology',
+  slot: html`<p>slotted content that should render</p>`,
 };
